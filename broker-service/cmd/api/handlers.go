d3406f414cab0337651805e086550cf641dda507
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2024-06-08 21:30:31 +0300

    add helpers to read and write json

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,24 +1,19 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
-type jsonRespnse struct {
-	Error bool `json:"error"`
-	Message string `json:"message"`
-	data any `json:"data"`
-}
+
 
 func(app *Config) broker(w http.ResponseWriter, r *http.Request){
 	payload := jsonRespnse {
 		Error: false,
-		Message: "Hello Broker",
+		Message: "Hello From broker",
 	}
 
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payload, nil)
+	
 }
+
+
---
+++ broker-service/cmd/api/helpers.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+
+type jsonRespnse struct {
+	Error bool `json:"error"`
+	Message string `json:"message"`
+	data any `json:"data omitEmpty"`
+}
+
+func(app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	maxBytes := 1048576
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(&data)
+	if err != nil {
+		return err 
+	}
+
+	err = dec.Decode(struct{}{})
+
+	if err != io.EOF {
+		return errors.New("body must have a single json object")
+	}	
+
+	return nil
+	
+	
+}
+
+
+
+func(app *Config) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error{
+	out, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func(app *Config) errJSON(w http.ResponseWriter, errObj error, status int) error{
+	payload := jsonRespnse {
+		Error: true,
+		Message: errObj.Error(),
+	}
+
+	err := app.writeJSON(w, status, payload, nil)
+	if err != nil {
+		return err
+	}
+
+	return err
+
+}
