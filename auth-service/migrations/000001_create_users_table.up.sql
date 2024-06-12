2c6e4acbd454688e5da90ee668b4295db2758045
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2024-06-12 13:28:33 +0300

    fix  migrations file

--- auth-service/migrations/000001_create_users_table.up.sql
+++ auth-service/migrations/000001_create_users_table.up.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXIST users(
+CREATE TABLE IF NOT EXISTS users (
     id bigserial PRIMARY KEY,
-    email citext UNIQUE NOT NULL,
-    first_name  text NOT NULL
-    last_name text NOT NULL
+    email text UNIQUE NOT NULL,
+    first_name  text NOT NULL,
+    last_name text NOT NULL,
     password character varying(60),
     user_active integer DEFAULT 0,
     created_at timestamp without time zone,
